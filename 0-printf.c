580d8f94ea8a7d985941948deba74f0419d62cda
Webprotekh <webprotekh@gmail.com>
2021-10-04 06:58:41 -0700

    two files, main.h and 0-printf.c, created

---
+++ 0-printf.c
@@ -0,0 +1,49 @@
+#include <stdarg.h>
+#include "main.h"
+
+/** _printf - The function to print to std output
+ * @format: The parameter for formating string
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+    int i = 0;
+    size_t amt = 0;
+    char *ptr = NULL;
+    va_list ap;
+
+    va_start(ap, format);
+    i = vsnprintf(ptr, amt, format, ap);
+    va_end(ap);
+
+    if (i < 0)
+        return NULL;
+
+    amt = (size_t) i + 1;
+    ptr = malloc(amt);
+    if (ptr == NULL)
+        return NULL;
+
+    va_start(ap, format);
+    i = vsnprintf(ptr, amt, format, ap);
+    va_end(ap);
+
+    if (i < 0)
+    {
+        free(ptr);
+        return NULL;
+    }
+    return ptr;
+
+
+    /*int charcount = 0;
+    int printint;
+    char intstr[10];
+    char *pprintstr;
+    va_list vlist;
+
+    va_start(vlist, format);
+    */
+}
