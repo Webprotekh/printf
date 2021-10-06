3972e4d01f3074553a25b5bebbebf54559bc7907
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-10-06 06:21:35 +0100

    updating _printf.c

--- _printf.c
+++ _printf.c
@@ -1,4 +1,21 @@
 #include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+char* _itoa(int val, int base){
+
+	static char buf[32] = {0};
+
+	int i = 30;
+
+	for(; val && i ; --i, val /= base)
+
+		buf[i] = "0123456789abcdef"[val % base];
+
+	return &buf[i+1];
+
+}
+
 /**
  * _printf - produces output according to a format
  *
@@ -10,6 +27,11 @@ int _printf(const char *format, ...)
 {
 	int s;
 	va_list ap;
+	char ch;
+	char *str;
+//	int no;
+//	char ch2;
+//	int len = strlen(str);
 
 	va_start(ap, format);
 	while (*format != '\0')
@@ -24,13 +46,16 @@ int _printf(const char *format, ...)
 		switch (*format)
 		{
 		case 's':
-			s = fputs(va_arg(ap, char *), stdout);
+			str = va_arg(ap, char *);
+			s = fputs(str, stdout);
 			break;
 		case 'c':
-			s = putchar(va_arg(ap, int));
+			ch = va_arg(ap, int);
+			s = fputc(ch, stdout);
 			break;
 		case '%':
-			s = putchar('%');
+			s = fputc('%', stdout);
+			break; 
 		}
 		format++;
 		}
